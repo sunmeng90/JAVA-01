94669246c075e5ef76b053ee674b79b6842fd6ad
Meng <sunmeng90@126.com>
2021-01-30 23:48:57 +0800

    week_03

---
+++ Week_03/周日作业/第一题/nio02/src/main/java/io/github/kimmking/gateway/filter/HeaderHttpResponseFilter.java
@@ -0,0 +1,11 @@
+package io.github.kimmking.gateway.filter;
+
+import io.netty.handler.codec.http.FullHttpResponse;
+
+public class HeaderHttpResponseFilter implements HttpResponseFilter {
+    @Override
+    public void filter(FullHttpResponse response) {
+        response.headers().set("kk", "java-1-nio");
+        response.headers().set("gateway-server", "netty");
+    }
+}
---
+++ Week_03/周日作业/第一题/nio02/src/main/java/io/github/kimmking/gateway/outbound/okhttp/OkhttpOutboundHandler.java
@@ -0,0 +1,132 @@
+package io.github.kimmking.gateway.outbound.okhttp;
+
+import io.github.kimmking.gateway.filter.HeaderHttpResponseFilter;
+import io.github.kimmking.gateway.filter.HttpRequestFilter;
+import io.github.kimmking.gateway.filter.HttpResponseFilter;
+import io.github.kimmking.gateway.outbound.httpclient4.NamedThreadFactory;
+import io.github.kimmking.gateway.router.HttpEndpointRouter;
+import io.github.kimmking.gateway.router.RandomHttpEndpointRouter;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class OkhttpOutboundHandler {
+
+
+    private OkHttpClient okclient;
+    private ExecutorService proxyService;
+    private List<String> backendUrls;
+
+    HttpResponseFilter filter = new HeaderHttpResponseFilter();
+    HttpEndpointRouter router = new RandomHttpEndpointRouter();
+
+    public OkhttpOutboundHandler(List<String> backends) {
+
+        this.backendUrls = backends.stream().map(this::formatUrl).collect(Collectors.toList());
+
+        int cores = Runtime.getRuntime().availableProcessors();
+        long keepAliveTime = 1000;
+        int queueSize = 2048;
+        RejectedExecutionHandler handler = new ThreadPoolExecutor.CallerRunsPolicy();//.DiscardPolicy();
+        proxyService = new ThreadPoolExecutor(cores, cores,
+                keepAliveTime, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(queueSize),
+                new NamedThreadFactory("proxyService"), handler);
+
+        okclient = new OkHttpClient.Builder()
+                .connectTimeout(1, TimeUnit.SECONDS)
+                .addInterceptor(chain -> {
+                    Request req = chain.request();
+                    System.out.println("request: " + req.url() + " at " + OffsetDateTime.now());
+                    Response resp = chain.proceed(req);
+                    System.out.println("response: " + resp.code() + " at " + OffsetDateTime.now());
+                    return resp;
+                })
+                .build();
+    }
+
+    private String formatUrl(String backend) {
+        return backend.endsWith("/") ? backend.substring(0, backend.length() - 1) : backend;
+    }
+
+    public void handle(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx, HttpRequestFilter filter) {
+        String backendUrl = router.route(this.backendUrls);
+        final String url = backendUrl + fullRequest.uri();
+        filter.filter(fullRequest, ctx);
+        proxyService.submit(() -> fetchGet(fullRequest, ctx, url));
+    }
+
+    private void fetchGet(final FullHttpRequest inbound, final ChannelHandlerContext ctx, final String url) {
+        Request req = new Request.Builder()
+                .url(url)
+                .header("Connection", "Keep-Alive")
+                .get()
+                .build();
+
+        Call call = okclient.newCall(req);
+        try {
+            Response response = call.execute();
+            handleResponse(inbound, ctx, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            call.cancel();
+        }
+    }
+
+    private void handleResponse(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx, final Response endpointResponse) throws Exception {
+        FullHttpResponse response = null;
+        try {
+            byte[] body = endpointResponse.body().bytes();
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(body));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", Integer.parseInt(endpointResponse.header("Content-Length")));
+
+            filter.filter(response);
+
+            for (String e : endpointResponse.headers().names()) {
+                response.headers().set(e, endpointResponse.header(e));
+                System.out.println(e + " => " + endpointResponse.header(e));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+            exceptionCaught(ctx, e);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    //response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+            ctx.flush();
+            //ctx.close();
+        }
+
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
